629546722fd1378c5a6b5ec78162b6e6d044e052
Szász István Zoltán <istvanszasz99@gmail.com>
2023-06-22 16:48:19 +0200

    Update Soruce Code Link

--- src/components/Projects/ProjectDatas.js
+++ src/components/Projects/ProjectDatas.js
@@ -4,7 +4,7 @@ export const projects = [
     description: "Personal portfolio website that contains all of my work, project and skill experiences and my resume.",
     image: 'https://raw.githubusercontent.com/istvanszasz99/portfolio/main/public/images/portfolio.jpg',
     tags: ['HTML', 'CSS', 'Javascript', 'React'],
-    source: 'https://github.com/istvanszasz99/PersonalWebPage.git',
+    source: 'https://github.com/istvanszasz99/portfolio',
     visit: '',
     id: 1,
   },
