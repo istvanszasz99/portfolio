58404b6705ae71689ac139600b3bf598b1821cb4
Szász István Zoltán <istvanszasz99@gmail.com>
2024-03-05 13:13:27 +0100

    Update 2024 March

--- src/components/Projects/ProjectDatas.js
+++ src/components/Projects/ProjectDatas.js
@@ -2,7 +2,7 @@
 export const projects = [
   {
     title: 'Portfolio Website',
-    description: "Personal portfolio website that contains all of my work, project and skill experiences and my resume.",
+    description: "Personal portfolio website that contains all of my work, project and skill experiences and my resume",
     image: 'https://raw.githubusercontent.com/istvanszasz99/portfolio/main/public/images/portfolio.jpg',
     tags: ['HTML', 'CSS', 'Javascript', 'React'],
     source: 'https://github.com/istvanszasz99/portfolio',
@@ -15,5 +15,29 @@ export const projects = [
     tags: ['HTML', 'Javascript', 'React', 'Tailwind CSS', 'Typescript'],
     source: 'https://github.com/istvanszasz99/WebstoreProject',
     id: 2,
+  },
+  {
+    title: 'OpenWeatherAPI project',
+    description: "OpenWeather API based project to learn how to use API-s in web development",
+    image: 'https://raw.githubusercontent.com/istvanszasz99/portfolio/main/public/images/APIscreenshot.jpg',
+    tags: ['HTML', 'Javascript', 'React', 'Tailwind CSS', 'API'],
+    source: 'https://github.com/istvanszasz99/OpenWeatherAPIproject',
+    id: 3,
+  },
+  {
+    title: '3D survival game',
+    description: "3D survival game made for university project work",
+    image: 'https://raw.githubusercontent.com/istvanszasz99/portfolio/main/public/images/Unreal4P.jpg',
+    tags: ['Unreal Engine 4'],
+    source: 'https://github.com/istvanszasz99/ObudaUniverityProjectWorkUE4',
+    id: 4,
+  },
+  {
+    title: 'Obuda University Thesis',
+    description: "Educational simulation of the university campus in a 3D environment using Unreal Engine 5",
+    image: 'https://raw.githubusercontent.com/istvanszasz99/portfolio/main/public/images/Unreal5T.jpg',
+    tags: ['Unreal Engine 5', 'Quixel Bridge', 'Twinmotion'],
+    source: 'https://github.com/istvanszasz99/ObudaUniverityThesis',
+    id: 5,
   },
 ];
