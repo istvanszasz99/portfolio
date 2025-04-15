0e37080e52a26ba95a6e5c3c8b0787119e8785d1
Szász István Zoltán <istvanszasz99@gmail.com>
2025-04-15 10:38:27 +0200

    Back-End Knowledge Update
    - Updated logoWall's
    - Updated resume with back-end certification
    - few minor changes in the descriptions on the webpage

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 const CategoryIcons = {
-  'Web Development': (
+  'Front-End Development': (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className='w-6 h-6 text-[var(--sec)] opacity-70'><path d="M21 3C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H21ZM20 11H4V19H20V11ZM20 5H4V9H20V5ZM11 6V8H9V6H11ZM7 6V8H5V6H7Z"></path></svg>
+  ),
+  'Back-End Development': (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className='w-6 h-6 text-[var(--sec)] opacity-70'><path d="M21 3C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H21ZM20 11H4V19H20V11ZM20 5H4V9H20V5ZM11 6V8H9V6H11ZM7 6V8H5V6H7Z"></path></svg>
   ),
   'Mobile Development': (
@@ -19,19 +22,27 @@ const SkillsList = () => {
   const [openItem, setOpenItem] = useState<string | null>(null);
 
   const skills = {
-    'Web Development': [
+    'Front-End Development': [
       'Dynamic Web Applications',
       'Development with React & TypeScript',
+      'Styling with Tailwind CSS',
       'Publishing with Vercel & Netlify',
+    ],
+    'Back-End Development': [
+      'REST APIs & CRUD operation management',
+      'Authentication & Authorization',
+      'Data validation & Error handling',
+      'Using MVC & MVT architecture',
+      'Development with Node.js, Django & Laravel',
+    ],
+    'Mobile Development': [
+      'Programming React Native mobile apps',
+      'Development with Android Studio',
     ],
     '3D Development': [
       'Level design & Programming',
-      'Architecture & Interior design with Twinmotion',
+      'World bulding & Interior design with Twinmotion',
       'Development with Unreal Engine 5',
-    ],
-    'Mobile Development': [
-      'React Native mobile apps',
-      'Development with Android Studio',
     ],
     'UI/UX Design': [
       'UI design with Figma',
